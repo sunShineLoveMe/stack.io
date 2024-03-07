220ef128a66f0c83ee5f52ffe1c00b448a210097
sunShineLoveMe <juneandrao@outlook.com>
2024-03-07 11:00:09 +0800

    first commit

---
+++ hello-stacks/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl">Hello Stacks</h1>
+    </div>
+  )
+}
+
+export default App
